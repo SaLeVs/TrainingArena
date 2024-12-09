8ac5f7e49034596c072b897b3658dc36be08f847
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-09 08:18:18 -0300

    v6
    Using methods for return values + challange for calculate how many attacks player recieve

--- Training Arena/Assets/Scripts/Methods.cs
+++ Training Arena/Assets/Scripts/Methods.cs
@@ -5,34 +5,52 @@ public class Methods : MonoBehaviour
     // void means that methods dont return values
 
     int x, y;
+    int playerHealth = 100;
+    int atackEnemy = 10;
+    int currentHealth;
 
-
-    private void Start()
+    public void Start()
     {
         x = 5;
         y = 10;
-        UpdateValues();
-        MultiplyValues(x,y);
+        currentHealth = 50;
+        Debug.Log(QuantityOfAttacks(playerHealth, atackEnemy, currentHealth));
     }
 
-   
+
+    int Subtraction(int valueA, int valueB)
+    {
+        // if you dont put return, the method will be underline with red trace
+
+       int result = valueA - valueB;
+       return result;
+    }
+    
+    int QuantityOfAttacks(int totalLife, int enemyAttack, int currentLife)
+    {
+
+        int quantityOfAttacks = (totalLife - currentLife) / enemyAttack;
+        return quantityOfAttacks;
+    }
+
+
     void UpdateValues()
     {
-        int resultado;
-        resultado = x + y;
+        int result;
+        result = x + y;
 
-        print(resultado);
+        print(result);
         // we can use print only for monobehaviour scripts
         // Debug log works on all unity
     }
 
     void MultiplyValues (int valueA, int valueB)
     {
-        int resultado;
+        int result;
 
-        resultado = valueA * valueB;
+        result = valueA * valueB;
 
-        Debug.Log(resultado);
+        Debug.Log(result);
         // we can use same names for local variables
 
     }
