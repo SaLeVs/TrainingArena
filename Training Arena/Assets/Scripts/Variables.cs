474427e3df38bbc2cbd58f92599d4149d61eb9c6
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-09 07:45:26 -0300

    v3
    Using the start and update method

--- Training Arena/Assets/Scripts/Variables.cs
+++ Training Arena/Assets/Scripts/Variables.cs
@@ -6,14 +6,35 @@ public class Variables : MonoBehaviour
     // we use variables for store data
 
     private int count = 0, x = 5;
+    float jumpForce = 5.5f;
+    string playerName = "Valverde";
 
     // = means that variable recive a value
 
-    private void Start()
+
+    private void Awake()
     {
-        count = 5;
-        x = x - 1;
-        Debug.Log(count);
+        count++;
     }
 
+    private void Start()
+    {
+        count = count + 5;
+        x = x - 1;
+        print(playerName);
+        print("jumpForce: " + jumpForce);
+        Debug.Log(count);
+        Debug.Log(x);
+    }
+
+
+    private void Update()
+    {
+        print(playerName);
+    }
+
+    private void DebugPlayerName()
+    {
+        print(playerName);
+    }
 }
