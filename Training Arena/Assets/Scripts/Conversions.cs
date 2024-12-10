08fc4374d245aa5c522505760c51034ac6600ea0
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-10 10:13:41 -0300

    v13
    Conversions

--- Training Arena/Assets/Scripts/Conversions.cs
+++ Training Arena/Assets/Scripts/Conversions.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class Conversions : MonoBehaviour
 {
     float number = 50f;
-    string name = "SaLeV";
-    int age = 19;
+    string names = "SaLeV";
+    int years = 19;
 
 
     private void Start()
     {
-        Debug.Log($"My name is {name} and I'm {age} years old")
+        Debug.Log($"My name is {names} and I'm {years} years old");
         int age = (int)number; // we can convert values using this (int)
         print(age);
     }
