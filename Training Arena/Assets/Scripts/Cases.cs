448ca6c6d35b0af53f30518d85100050dea4b322
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-10 21:36:15 -0300

    v14
    More off if and else conditions

---
+++ Training Arena/Assets/Scripts/Cases.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Cases : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log(1 == 1); // we can check infos on debug
+
+        if (1 < 2)
+            Debug.Log("false"); // we can make a if with one line
+
+    }
+
+
+}
