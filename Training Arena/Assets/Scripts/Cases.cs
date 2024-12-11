d14b228e565207c6c08f77593dbdbab199d3e813
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-10 21:54:54 -0300

    v15
    Switch case and enums

--- Training Arena/Assets/Scripts/Cases.cs
+++ Training Arena/Assets/Scripts/Cases.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class Cases : MonoBehaviour
 {
-    
+
+    public enum Personagens
+    {
+        IronMan,
+        Groot,
+        SpiderMan
+    }
+
     void Start()
     {
         Debug.Log(1 == 1); // we can check infos on debug
@@ -10,7 +17,25 @@ public class Cases : MonoBehaviour
         if (1 < 2)
             Debug.Log("false"); // we can make a if with one line
 
+        SwitchTest(Personagens.IronMan); // we dont need to create a global variable, we can change on loop call
     }
 
-
+    private void SwitchTest(Personagens character)
+    {
+        switch (character)
+        {
+            default: // default is important for localize bugs
+                Debug.Log("Character not recognized");
+                break;
+            case Personagens.IronMan:
+                Debug.Log("I`m Iron Man");
+                break;
+            case Personagens.Groot:
+                Debug.Log("I`m Groot");
+                break;
+            case Personagens.SpiderMan:
+                Debug.Log("I can`t save Gween");
+                break;
+        }
+    }
 }
