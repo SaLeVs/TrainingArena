ddbd17eb0885f5548a66fc879f987864d9bb5cf4
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-11 10:21:06 -0300

    v18
    Testing renders assets

---
+++ Training Arena/Assets/Scripts/RendersExplanations.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RendersExplanations : MonoBehaviour
+{
+    /*
+      Render pipelines
+
+        Built in - Old and have some issues
+        Universal render pipeline - adaptable, better performance, intented to be universal, runs on every plataform, maximium compatibility
+        high definition render pipeline - maximum fidelity, intender for consoles and high end pcs
+      
+        URP and HRP have shaders graph and visual effect graph
+
+        have diferrence when you add a object on scene and when you add a object on project
+      
+        We can change the shaders on graphics on project settings
+    
+    */
+}
