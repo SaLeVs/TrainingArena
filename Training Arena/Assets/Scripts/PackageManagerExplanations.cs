83450a38ff3aedce7e1b800964a09891dd59b01e
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-11 22:19:27 -0300

    v20
    Just a bit of package manager

---
+++ Training Arena/Assets/Scripts/PackageManagerExplanations.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PackageManagerExplanations : MonoBehaviour
+{
+    /*
+     packages = Realese, experimental, pre realese
+     
+     we can add packages with links
+
+     only use realese packages
+
+    on sprites, its very important use full rect
+     */
+
+}
