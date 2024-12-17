791c28c76f68f1939f47e35d72757a827143f0a5
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-17 09:58:24 -0300

    v30
    Static explanation

---
+++ Training Arena/Assets/Scripts/Static.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Static : MonoBehaviour
+{
+    // define wheter something belongs to the class itself or an instance of that class
+    // if you make something static only exist on that class
+    // static functions can acess static variables
+}
