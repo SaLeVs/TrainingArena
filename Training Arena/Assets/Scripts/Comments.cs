f7b6468ea9aa99d9d2a47a729d4789f524327dfe
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-17 07:31:36 -0300

    v26
    Comments

---
+++ Training Arena/Assets/Scripts/Comments.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Comments : MonoBehaviour
+{
+    /*
+ 
+     // focus on why, not how
+
+     if your variables and functions have good names, we dont need comment
+     we can use // or */
+
+     
+}
