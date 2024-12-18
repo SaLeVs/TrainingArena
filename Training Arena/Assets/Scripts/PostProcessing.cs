3dfe391ef16f058f441a25106e9275088160d2c0
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-18 07:55:27 -0300

    v32
    Find objects and change properties

--- Training Arena/Assets/Scripts/PostProcessing.cs
+++ Training Arena/Assets/Scripts/PostProcessing.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PostProcessing : MonoBehaviour
 {
@@ -9,10 +11,19 @@ public class PostProcessing : MonoBehaviour
         Put vignette and put bloom
         
         For use glow its simple create material and active on camera, render pipiline asset and put emission
-        
-        
-        
-        
-        
-     */
+        Color adjustments its important for create saturation 
+        We can add volumes for locals, and change the configuration specify for this local, we just need create a new game object and put Local, and add a new trigger collider
+        We can change the layer that affects Post process on Camera     */
+
+    private void Start()
+    {
+        Volume volume = GetComponent<Volume>(); // With this we can get all components on volume
+
+        if (volume.profile.TryGet<Bloom>(out Bloom bloom)) // on debug mode and double clicked on profile you can get the name of class, and with this script ou can change all values and have explicative for it
+        {
+            bloom.intensity.value = 10f;
+        }
+
+    }
+
 }
