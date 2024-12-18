316a1ef73425ec81146e4633454c5549cb15e919
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-18 07:32:16 -0300

    v31
    New glow material + post processing

---
+++ Training Arena/Assets/Scripts/PostProcessing.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PostProcessing : MonoBehaviour
+{
+    /*
+        You need to create a post process object
+        Put Volume on object
+        Enable post processing on Camera
+        Put vignette and put bloom
+        
+        For use glow its simple create material and active on camera, render pipiline asset and put emission
+        
+        
+        
+        
+        
+     */
+}
