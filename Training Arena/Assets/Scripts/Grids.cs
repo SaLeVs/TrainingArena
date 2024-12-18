ce0cd808b5a12a68d7d83b9df2753a257cb2132c
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-12-18 09:05:14 -0300

    v36
    Using grids on unity

---
+++ Training Arena/Assets/Scripts/Grids.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Grids : MonoBehaviour
+{
+    // We can toggle grid and snap effect
+    // if we press control can control tiny movement
+    // we can snap objetcts automatically on grid, just open snap options
+    // We have so much grid shortcuts for afacillit level design
+    // We can change grid color
+}
