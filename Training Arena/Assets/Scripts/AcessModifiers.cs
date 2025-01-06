88a17395ddc3fc33ad0ac5bc664e1a73b8d87733
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-06 07:25:58 -0300

    v40
    Acess modifiers

---
+++ Training Arena/Assets/Scripts/AcessModifiers.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AcessModifiers : MonoBehaviour
+{
+    // Manage how accesible a certain variable/function should be
+    // controls waht kind of acess we have with something
+    // we use private for limit acessibility the code for create a clean code
+    // we make all private and only use public if have a good reason
+
+
+    private void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+}
