a8505ea24abac961b836b9f07bdefe2c382f3421
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-06 09:17:43 -0300

    v44
    Refactoring

---
+++ Training Arena/Assets/Scripts/Refactoring.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Refactoring : MonoBehaviour
+{
+    // The constant process that helps you keep your code quality as high as possible
+
+    // The act of rewriting or reorganizing code its a constant never eding process
+    // if you ever come up with a better way to do something or just a better way to organize some class or logic then take some time and refactor that code
+    // writing semi dirty code is okay at first. What is not okay is leaving it dirty
+    // first get it working, then refactor the code to get it working properly
+
+    // One diference betwen begginers and intermediates is reafctoring
+   
+}
