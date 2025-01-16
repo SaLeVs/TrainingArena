cdcfd21472a2a6fd57c9e49f06ee5ebdec7db271
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-16 09:19:11 -0300

    v72
    Design patterns

---
+++ Training Arena/Assets/Scripts/C#_Study/DesignPatterns.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DesignPatterns : MonoBehaviour
+{
+    // Multiple pre-made solutions to solve commom problems
+    // Comand Pattern: Encapsulate a request as an object, thereby allowing for parameterization of clients with different requests, queuing requests, and logging requests, and supporting undoable operations.
+    // Observer Pattern: Define a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically.
+    // Comand can be good for undo actinos
+    // Observer can be good for UI updates, use of events
+
+    // we dont need to know all the patterns, but we need to know when to use them
+    // we can use patterns to solve problems in a more organized way
+
+
+}
