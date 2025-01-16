5bf2f0beb3576e776cff3ecec54683bd9b1a7d2e
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-15 22:59:59 -0300

    v68
    Constants and readonly

---
+++ Training Arena/Assets/Scripts/C#_Study/ConstantsReadOnly.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ConstantsReadOnly : MonoBehaviour
+{
+    // Variables that have a value that will never change
+
+    int age;
+    private const float PLAYER_SPEED = 5f; // Constants are always uppercase
+
+    // We cant declare a constant without a value
+    // Constants works at compile time
+
+    private readonly float PLAYER_HEALTH = 100f; // Readonly works at runtime
+    // We can create variables of class using readonly
+
+    // can be useful
+
+    private void Start()
+    {
+        Debug.Log(PLAYER_SPEED);
+        //  PLAYER_HEALTH = 50f; // We cant change the value of a constant
+    }
+}
