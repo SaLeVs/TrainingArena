eb5405476e7ed10fe6ac7415eb07d539ee097017
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-22 23:51:45 -0300

    v78
    NameOf

---
+++ Training Arena/Assets/Scripts/C#_Study/NameOf.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NameOf : MonoBehaviour
+{
+    // Get the name of a Type of field or function, without hardcoding strings
+
+    private void Start()
+    {
+        string playerClassName = nameof(Player); // name of is a keyword that returns the name of something
+        // can be useful for avoid hardcoding strings
+        // But, the most value about this is when we use it in the refactoring, for example, if the name player was "Player", if we change the name of the class, the string dont will change
+        // but the name of will change
+        // work for fields, classes, functions, etc
+    }
+    public class Player
+    {
+        public int speed;
+
+        public bool IsGrounded()
+        {
+            return true;
+        }
+    }
+}
