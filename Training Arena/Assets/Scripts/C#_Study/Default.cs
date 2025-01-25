b508e802af4d23e33b9347b7fead5130880f767f
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-24 23:33:56 -0300

    v80
    Default

---
+++ Training Arena/Assets/Scripts/C#_Study/Default.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Default : MonoBehaviour
+{
+    // Get the zero-equivalent value of a certain type, whatever it may be
+
+    private void Start()
+    {
+        int i = 1;
+        Debug.Log(default(int)); // 0
+    }
+}
