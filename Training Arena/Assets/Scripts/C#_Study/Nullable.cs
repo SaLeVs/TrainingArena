64476befc7e2ae11349047f5acba0896b7a6759d
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-27 08:06:13 -0300

    v87
    Nullable

---
+++ Training Arena/Assets/Scripts/C#_Study/Nullable.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Nullable : MonoBehaviour
+{
+    // Make any type support null, even value types
+
+    // we cant assign null to value types, but c# has a solution for this
+    int? i = null; // int? is a shorthand for Nullable<int>
+    Nullable<int> j = null; // this is the same as the above line, but we need to use the system namespace
+
+    private void Start()
+    {
+        Setup(); // we can call the method without passing any value beacuse we have set the default value to null instead of 0, can be good for optional parameters
+    }
+
+    private void Setup(int? i = null) // this make more sense than setting the default value to 0
+    {
+        Debug.Log(i);
+
+    }
+}
