b4c703a8f0894cdba1b02707280bf3fb213e3699
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-27 07:57:45 -0300

    v86
    Command line args

---
+++ Training Arena/Assets/Scripts/C#_Study/MainAndCommandLineArgs.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MainAndCommandLineArgs : MonoBehaviour
+{
+    // How to make more complex console apps with arguments
+
+    // all engine have a start and break point, and is simple for unity
+    // the start point is the Start() method
+    // the break point is when player press the quit button
+    // normally people make a console app with a Main() method, because they dont have a UI to see the program running
+    // we can use args to pass arguments to the program, like a file path, or a number, or a string, this in unity is not common, but is possible
+    // in c#, we can have only one start point, so we can have only one Main() method, but we can acess and set where the start point 
+
+}
