58c6b6dcbfb3de1eff7028a98431db6dc8857751
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-27 09:15:00 -0300

    v90
    Enum flags

---
+++ Training Arena/Assets/Scripts/C#_Study/EnumFlags.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class EnumFlags : MonoBehaviour
+{
+    // Work with enums as a bitmask
+
+    [Flags]
+    public enum  Abillity : byte // to save memory
+    {
+        None    = 0,
+        jump    = 1 << 0,
+        Sprint  = 2 << 1,
+        Shoot   = 4 << 2,
+        Fly     = 8 << 3
+    }
+
+    private void Start()
+    {
+        Abillity playerAbilities = Abillity.jump | Abillity.Shoot;
+        Debug.Log(playerAbilities);
+
+        Debug.Log(playerAbilities.HasFlag(Abillity.jump)); // True
+    }
+}
