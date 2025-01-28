da295dd11eae237db21d50d937dedf2c51fde292
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-28 08:31:50 -0300

    v95
    Dynamic

---
+++ Training Arena/Assets/Scripts/C#_Study/Dynamic.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Dynamic : MonoBehaviour
+{
+    // Write code against a type that does note exist at compile time
+
+    dynamic d; // dynamic type means it can be anything
+               // works on runtime, not compile time
+
+    private void Start()
+    {
+        d = new object();
+        d.Test();   // no error at compile time, but error at runtime
+    }
+
+    // we can use dynamic with json data
+
+}
