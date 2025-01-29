5557219f29d3ae6241aa2179b585779a9f1b6e61
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-29 08:51:49 -0300

    v103
    DLL import

---
+++ Training Arena/Assets/Scripts/C#_Study/DLLImport.cs
@@ -0,0 +1,16 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DLLImport : MonoBehaviour
+{
+    // How to interact with external libraries
+
+    //[DllImport("user32.dll")] // its more complex than this, but this is the basic idea
+    //public static extern bool MessageBeep(uint uType);
+
+
+    //private void Start()
+    //{
+    //    MessageBeep(0);
+    //}
+}
