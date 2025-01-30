9b5ab4313ecd723fdeb08ef1ba4211f301199227
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-30 09:43:31 -0300

    v111
    Pro Builder

---
+++ Training Arena/Assets/Scripts/Tools_Study/ProBuilder.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ProBuilder : MonoBehaviour
+{
+    // Do 3D Modelling directly inside Unity. Extremely useful for quickly whhiteboxing levels
+    // we need to add on package manager
+
+    // the most crazy about this powerfull tool is that we can modify 3d models that already have created on other software, just ProBuilderize it  
+    // The uv editor tool is also very useful to modify the UVs of the models
+
+
+}
