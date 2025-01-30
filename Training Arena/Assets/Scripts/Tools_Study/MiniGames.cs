79e569a92d97875ca4a5b5ac4ef885368dba95be
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-30 09:18:58 -0300

    v110
    Minigames

---
+++ Training Arena/Assets/Scripts/Tools_Study/MiniGames.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class MiniGames : MonoBehaviour
+{
+    // Fully working mini-games made by unity for you to lern from Or use as base for quickly making prototypes
+    // we can learn much with microgames
+
+}
