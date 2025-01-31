2b9df85fff1d3ae083ae5e838e088334aae3e015
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-31 09:16:20 -0300

    v114
    Adressables

---
+++ Training Arena/Assets/Scripts/Tools_Study/Resources.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System;
+
+
+public class Resources : MonoBehaviour
+{
+    // Super simple way to load assets during runtime
+
+    private void Start()
+    {
+       // Resources.Load<Transform>("Wall");
+    }
+
+    // now we use adressable assets for optimization
+}
