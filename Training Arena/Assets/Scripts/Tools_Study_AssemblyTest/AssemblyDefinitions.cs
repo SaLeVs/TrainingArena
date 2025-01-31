eb8f9d775bb46346c806aeb916e4dceedbe87b7b
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-31 08:56:17 -0300

    v113
    Assembly definitions

---
+++ Training Arena/Assets/Scripts/Tools_Study_AssemblyTest/AssemblyDefinitions.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AssemblyDefinitions : MonoBehaviour
+{
+    // Write beter cleaner code and improve compile times
+    // Organazing scipts
+    // if you create new asssembly definition, you need to put on other assemblye the definition of the other assembly
+    // if the script is not in the same folder, you can create a assembly reference
+
+
+
+}
