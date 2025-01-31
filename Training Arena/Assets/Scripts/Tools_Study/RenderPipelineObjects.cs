795b57d185e45db868134b1d47c51d4ad9fd61e1
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-01-30 23:39:40 -0300

    v112
    Render pipeline objects

---
+++ Training Arena/Assets/Scripts/Tools_Study/RenderPipelineObjects.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class RenderPipelineObjects : MonoBehaviour
+{
+    // Create awesome effects by rendering specific objects 
+    // the objects on layer mask will be rendered with the material that you choose on RenderObjects
+    // we learn about fram debuger, and how to use it to see the objects that are being rendered
+    // Dither can be good for shader graph effects
+}
