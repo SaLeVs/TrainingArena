f04b630f08d2a3679451b217ca1156ac57f2790d
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-01 21:58:06 -0300

    v116
    Pivot-Center x Local-Global

---
+++ Training Arena/Assets/Scripts/Tools_Study/PivotCenterAndLocalGlobal.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PivotCenterAndLocalGlobal : MonoBehaviour
+{
+    // Learn what the pivot is and how to change it
+
+    // just to know about the diferences between the pivot and center button
+    // the pivot is the point that the object rotates around, and the center is the point that the object is placed in the world
+
+}
