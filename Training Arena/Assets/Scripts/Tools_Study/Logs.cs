14be59c1d0d0e8d968622edde43593340f630ef8
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-01 22:13:48 -0300

    v119
    Logs

---
+++ Training Arena/Assets/Scripts/Tools_Study/Logs.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Logs : MonoBehaviour
+{
+    // Find out what happened. Editor Logs, build Logs
+
+    // we can acess the logs of build or games, interesting for see wath happened if we have a error on build or game
+    // In documentation we can see the road for the logs, and how to acess it
+
+}
