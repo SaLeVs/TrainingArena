8288dec7935315d3d64155f0753e972f31375e9f
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-02 20:35:50 -0300

    v120
    Nav mesh changes on unity 6

---
+++ Training Arena/Assets/Scripts/Tools_Study/NavMeshExplain.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class NavMeshExplain : MonoBehaviour
+{
+    // Unity`s built-in Pathfinding solution. Bake a navigation mesh and your agents will traverse it
+
+    // now you can create diffenret agent types and set their properties, like height, radius, speed
+
+}
