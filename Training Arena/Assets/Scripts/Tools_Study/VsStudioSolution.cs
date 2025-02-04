5825ce84fc1f34e491393a5cdff832d41b4ca6be
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-03 22:51:38 -0300

    v121
    Visual studio solutions

---
+++ Training Arena/Assets/Scripts/Tools_Study/VsStudioSolution.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class VsStudioSolution : MonoBehaviour
+{
+    // Set up your visual studio so it recognizes imported packages. Great for exploring all source code of the unity packages.
+    // we use this for see functions on packages on Csharp
+    // can be useful, but not essential
+}
