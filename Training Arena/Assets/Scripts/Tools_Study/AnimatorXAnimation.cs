ce6f3c5e6ace1ba5ce06a321d97de293b56acd95
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-08 13:59:40 -0300

    v128
    Animator vs Animation components

---
+++ Training Arena/Assets/Scripts/Tools_Study/AnimatorXAnimation.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AnimatorXAnimation : MonoBehaviour
+{
+    // What is the difference, wich one use
+    // for change the animation for legacy, we have to go on window -> Animation -> Animation
+    // after create, click on the animation and active the debug mode, so you can set for legacy or not
+    // if you have a single state animation, can be useful use the animation component
+
+}
