803262559b867bc7443667f24bbb2c44b5f37698
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-09 16:14:55 -0300

    v132
    Animation Avatar

---
+++ Training Arena/Assets/Scripts/Tools_Study/AnimationAvatar.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AnimationAvatar : MonoBehaviour
+{
+    // Use any humanoid animations with any humanoid charactersc
+    // change the avatar allow you use any humanoid animations with any humanoid characters
+    // its crazy how much thing we can do using the avatar armature configuration
+    // we can create a avatar on rig section, and configure the animation type for humanoid. and click on create
+    // can be good import from mixamo using without skin, for use the avatar on the rig section
+    // we can combine different avatar using different layers of animations
+}
