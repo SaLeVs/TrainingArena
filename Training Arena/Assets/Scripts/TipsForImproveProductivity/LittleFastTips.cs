434a399c8e3fdb657ccaa44410958fbf80075dee
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-12 14:59:16 -0300

    v137
    Fast tips

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/LittleFastTips.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LittleFastTips : MonoBehaviour
+{
+    // Acess debug mode on hierarchy can be good
+    // we can ocult layer and lock, for see exactly what we want
+    // numbering sheceme is for better organization type, we can modify the number of multiples gameobjects
+    // We can use EditorOnly tag for objects that we want to see only in editor not in build
+    //  we can change the color of unity on edit -> preferences -> colors
+    // when setup camera we can align with the scene view, for better visualization: GameObject -> Align with view
+
+}
