87ffbe1783cf4e0e2a764fc3586724245179ebd1
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-12 13:35:42 -0300

    v135
    Preset manager

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/Presets.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Presets : MonoBehaviour
+{
+    // Presets enable you to customize the default state of anything in your inspector
+    // to create a preset, set the values of the inspector to what you want, then click the gear icon in the top right of the inspector and select "save as preset"
+    // We have the preset manager, we can create a preset for component, every time we add a component, automatically apply the preset
+    // we just need to create a simple preset and attach on preset manager for a component, dont use the nameBar
+}
