1748338d988b3889bb59e9e1d42d4ef12ac9d6d0
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-12 13:18:22 -0300

    v134
    Focused inspectors

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/FocusedInspectorsTip.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class FocusedInspectorsTip : MonoBehaviour
+{
+    // Opening multiple focused inspectors at the same time allows you to reference multiple gameobjects while making changes to the scene
+    // To open a focused inspector, right-click on a gameobject in the hierarchy and select "properties"
+}
--- Training Arena/Assets/Scripts/TipsForImproveProductivity/ShortcutManagerTips.cs
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/ShortcutManagerTips.cs
@@ -5,4 +5,6 @@ public class ShortcutManagerTips : MonoBehaviour
     // Edit -> Shortcuts
     // We can save profiles for different projects
     // pay attetion on actions that we repeat a lot and attach shortcuts to them
+    // Z - toggle pivot mode
+    // P - edit prefab in contex
 }
