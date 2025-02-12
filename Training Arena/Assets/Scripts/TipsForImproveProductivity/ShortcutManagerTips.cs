56baa7095d119fc13ba822b065bc8875872967cf
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-12 13:14:15 -0300

    v133
    Shortcut manager

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/ShortcutManagerTips.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ShortcutManagerTips : MonoBehaviour
+{
+    // Edit -> Shortcuts
+    // We can save profiles for different projects
+    // pay attetion on actions that we repeat a lot and attach shortcuts to them
+}
