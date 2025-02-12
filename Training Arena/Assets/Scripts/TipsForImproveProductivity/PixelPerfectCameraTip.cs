815ade2441b988dfec8f4f1297d05a21d7cbbe8e
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-12 15:35:51 -0300

    v139
    Pixel perfect Camera + 3d tilemap

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/PixelPerfectCameraTip.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PixelPerfectCameraTip : MonoBehaviour
+{
+    // This package contain a pixel perfet camera that ensures your pixel art looks crisp and clear at different resolutions
+    // use the PSD importer package to import your pixel art
+    // extra tip, we can use the tilemap extras package for use tilemaps for 3d games
+}
