75b8fb5cb3471cca470768ab7a6f282fc0cd921e
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-13 17:44:30 -0300

    v147
    Script template

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/NewScriptStyleTip.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class NewScriptStyleTip : MonoBehaviour
+{
+    // its crazy, but we can modify the class default style
+    // arquivos de programa/unity/hub/editor/your_version_of_unity/editor/data/resources/scripttemplates/1-Scripting__MonoBehaviour Script-NewMonoBehaviourScript.cs
+    // works, open on VSCode, change the style, save and close, retry as admin and will work
+}
