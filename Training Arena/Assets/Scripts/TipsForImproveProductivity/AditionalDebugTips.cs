16837a96868788acce4389f598fef2a02a9bb583
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-13 18:48:25 -0300

    v150
    Some debug tips

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/AditionalDebugTips.cs
@@ -0,0 +1,21 @@
+using UnityEditor.Splines;
+using UnityEngine;
+
+public class AditionalDebugTips : MonoBehaviour
+{
+    // use Debug.Break() to pause the game in the editor
+    // Debug.DrawLine() to draw a line in the scene view
+    // Debug.DrawRay() to draw a ray in the scene view
+
+    // for use breakpoints 
+
+    private int i;
+    private void Start()
+    {
+        i = 5; // we just click on the left side of the line number to add a breakpoint
+
+
+        // after that we click on "anexar ao unity"
+        // we can see the value of the variable i and a bunch of information
+    }
+}
