fe994c94da51301043bf4eb0d18548cd4cde4ee2
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-13 16:31:34 -0300

    v144
    Decals

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/DecalsTip.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class DecalsTip : MonoBehaviour
+{
+    // Decals are useful for print images on every surface, they addapt to the surface
+    // we need to add a render freture to scene
+    // after that we add a decal on rendering, decal - decal projector
+    // we need to create a material of model shadergraphs/decals
+    // and we can add a normal and a mask texture for these material
+    // after that we can add a decal to the scene
+}
