d3467deecca8b4b5acd104e7f118a07bbfb9b472
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-12 21:52:39 -0300

    v141
    Snap Tips

---
+++ Training Arena/Assets/Scripts/TipsForImproveProductivity/SnapTips.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SnapTips : MonoBehaviour
+{
+    // vertex snapping is a technique that allows you to snap the vertices of a mesh to a grid
+    // hold down V while moving the vertices to snap them to the grid
+    // we can use shift + V to snap the vertices to the grid with a higher increment
+    // we can increase the grid size and we have shortcuts for that
+
+}
