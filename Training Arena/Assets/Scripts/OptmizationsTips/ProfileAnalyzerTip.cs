fd58a0270a8acc396b5a7f2ca871b9ffcbf1f80b
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-14 17:15:55 -0300

    v156
    Profily analyzer

--- Training Arena/Assets/Scripts/OptmizationsTips/ProfileAnalyzerTip.cs
+++ Training Arena/Assets/Scripts/OptmizationsTips/ProfileAnalyzerTip.cs
@@ -6,5 +6,12 @@ public class ProfileAnalyzerTip : MonoBehaviour
     // Monitors performance 
     // compare two sets of data
     // Its used for identify where optimization techniques are required
+    // We need to instal the package from the package manager
+    // this works alongside profiler so we need to open both
+    // profiler is used for inspect individual frames
+    // profile analyzer is used for inspect a range of frames and automatically prepares analytical data arranged in the sections we see here 
+    // Change for self can show more data and specific things
+    // we can see all threads
+    // we can compare data, using compare and load and save some tests
     // 
 }
