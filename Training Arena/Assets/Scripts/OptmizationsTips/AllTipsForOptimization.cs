7c177190a670a4bd50c62bb899d51a4f659945fd
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-14 16:37:26 -0300

    v155
    Packages

--- Training Arena/Assets/Scripts/OptmizationsTips/AllTipsForOptimization.cs
+++ Training Arena/Assets/Scripts/OptmizationsTips/AllTipsForOptimization.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public class AllTipsForOptimization : MonoBehaviour
 {
     // Unity recommends using the URP for developing mobile games, xr and web
-    // 
+    // We dont need wait for significant performance problems to start profiling
+
 }
---
+++ Training Arena/Assets/Scripts/OptmizationsTips/ProfileAnalyzerTip.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ProfileAnalyzerTip : MonoBehaviour
+{
+    // Analyzes data across a range of frames
+    // Monitors performance 
+    // compare two sets of data
+    // Its used for identify where optimization techniques are required
+    // 
+}
