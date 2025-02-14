088cd229819d10a33d3e52167cda8e34c759b946
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-14 19:15:10 -0300

    v160
    Adaptative performance

---
+++ Training Arena/Assets/Scripts/OptmizationsTips/AdaptativePerformanceTip.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AdaptativePerformanceTip : MonoBehaviour
+{
+    // We need to instal the package "Adaptative Performance" to use this tip
+    // its for check the performance of the game and adapt it to the device
+    // we can change things and configs in runtime for realize tests
+    // for use, its just to play and abble on control panel
+    // we can control the performance of game using scripts and inheriting from "AdaptativePerformanceManager"
+}
