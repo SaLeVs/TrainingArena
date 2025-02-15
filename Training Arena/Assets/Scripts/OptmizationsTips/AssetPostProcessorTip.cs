f342351965094691ad35eec4aff19ced8358dab1
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-15 11:35:33 -0300

    v162
    Asset Post Processor

---
+++ Training Arena/Assets/Scripts/OptmizationsTips/AssetPostProcessorTip.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AssetPostProcessorTip : MonoBehaviour
+{
+    // We have a way to create a script that automate the process of importing assets on unity
+    // its similar to presets, but we can modify more settings and work with code and automaticaly apply the settings
+    // For use we have to inherit from AssetPostProcessor class
+    // We have voids line OnProcessModel
+    // Its more complex but in big projects we use for automate for artists
+}
