9d62a69105b2ea774bf135d19f1c0a42f620e57b
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-15 14:44:38 -0300

    v169
    Frustum culling

---
+++ Training Arena/Assets/Scripts/OptmizationsTips/FrustumCullingTip.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FrustumCullingTip : MonoBehaviour
+{
+    // Frustum culling its a script, so create a gameobject and add the component
+
+    // disable auto catch camera and set the camera that will be used
+    // Add a Frustum culling object for the object that will be culled
+
+    // after build edge 
+    // we can resize the edges and the object will be culled
+
+    // its simple
+
+    
+}
