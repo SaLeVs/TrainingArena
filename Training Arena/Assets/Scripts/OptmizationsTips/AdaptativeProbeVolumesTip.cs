5cd95ee254afc9d36da2079445b40ae048c3fbf6
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-15 23:35:24 -0300

    v172
    Adaptative probe volumes

---
+++ Training Arena/Assets/Scripts/OptmizationsTips/AdaptativeProbeVolumesTip.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AdaptativeProbeVolumesTip : MonoBehaviour
+{
+    // APVs wich provide a sophisticated solution for handling global illumination in Unity, allowing for dynamic and efficient lighting in complex scenes.
+
+    // APVs can optmize both performance and visual quality particularly on mobile and lower end services, while offering advanced capabillities for high end platforms
+
+    // first step, is create a lighting seetings asset, and after change the lighing mode for Baked indirect
+    // after that, will create a nwe adaptive probe volume, and set the volume size, and the resolution, and the number of probes
+    // make global on light probes
+    // After that we just need to bake the probes
+
+    // according with the youtuber, the difference between the light probes and the adaptive probe volumes, is that the adaptive probe volumes are more optimized for mobile devices, and manually configs
+    
+}
