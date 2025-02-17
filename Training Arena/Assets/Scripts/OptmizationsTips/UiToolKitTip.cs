1fd7f97e6123740ad4cd5a67b134b46b048688fa
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-17 07:22:02 -0300

    v178
    Ui toolkit things

---
+++ Training Arena/Assets/Scripts/OptmizationsTips/UiToolKitTip.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UiToolKitTip : MonoBehaviour
+{
+    // It offers improved performance over unity UI, is tailored for maximum performance and reusability
+    // One of its key benefits is that it uses a highly optimized rendering pipeline that is specifically designed for UI elements
+
+    // On assets folder, right click -> Create -> UI Toolkit -> Editor window
+    // In UI Toolkit Editor Window Creator, enter SimpleCustomEditor in the C# box.
+    // Keep the UXML checkbox selected and clear the USS checkbox.
+    // Select Confirm
+    // To open the Editor window, select Window > UI Toolkit > SimpleCustomEditor.
+
+    // But for build things Window > UI Toolkit > UI Builder
+}
