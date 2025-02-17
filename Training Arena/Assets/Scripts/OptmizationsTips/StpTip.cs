62680fd14064b9b945fb27d4239648a1ea9306c3
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-16 22:06:32 -0300

    v174
    Spatial-Temporal Post-processing

---
+++ Training Arena/Assets/Scripts/OptmizationsTips/StpTip.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class StpTip : MonoBehaviour
+{
+    // Spatial-Temporal post-processing (STP) is a new technique that combines spatial and temporal anti-aliasing
+    // Is designed to enhance visual quality across a wide range of plataforms, from mobile devices to high-end consoles
+    // Offering high-quality content scaling without the need for changes to existing content
+    // For enable STP: Select the URP asset in project window -> Quality -> Upscaling filter -> Spatial-Temporal post-processing
+
+    // STP is a great way to improve the visual quality of your game without sacrificing performance
+}
