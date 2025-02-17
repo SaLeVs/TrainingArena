d235beda54fe15ae0b42d2a054659c0e0fe7a335
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-17 06:24:01 -0300

    v176
    More tips for optmization

--- Training Arena/Assets/Scripts/OptmizationsTips/AllTipsForOptimization.cs
+++ Training Arena/Assets/Scripts/OptmizationsTips/AllTipsForOptimization.cs
@@ -8,8 +8,18 @@ public class AllTipsForOptimization : MonoBehaviour
 
     // Avoid mobile native resolution
     // We can use Screen.SetResolution(width, height, false) to lower the output resolution and regain some performance
-    
+
     // Limit use of Cameras
     // Only use the Camera components required for rendering . On lower-end mobile, each camera can use up to 1ms of CPU time
-    
+
+    // Limit post-processing effects
+    // Full-screen post-processing effects can be expensive, so use them sparingly
+
+    // Optimize SkinnedMeshRenderers
+    // Make sure that every object using a SkinnedMeshRenderer requires it
+    // If a gameobject only needs animation some of time, use the bakeMesh function to freeze the skinned mesh in a static pose, then swap to a simpler meshRenderer at runtime
+
+    // Minimize reflection probes
+    // This component create realistic reflections, but they can be expensive
+    // Use low-resolution cubemaps, culling masks and texture compression to improve runtime performance
 }
