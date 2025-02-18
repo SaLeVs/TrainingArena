4e58a866e72340b869b914a084abd25e5f6aa8bf
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-18 06:03:07 -0300

    v179
    More optimization tips about Ui toolkit

--- Training Arena/Assets/Scripts/OptmizationsTips/UiToolKitTip.cs
+++ Training Arena/Assets/Scripts/OptmizationsTips/UiToolKitTip.cs
@@ -12,4 +12,23 @@ public class UiToolKitTip : MonoBehaviour
     // To open the Editor window, select Window > UI Toolkit > SimpleCustomEditor.
 
     // But for build things Window > UI Toolkit > UI Builder
+
+    // Use efficient layouts
+    // Refer to using layout groups provided by UI toolkit, instead of manually positions and resizing UI elements
+
+    // Avoid expensive operations in Update
+    // minimize heavy operations like UI element creation, manipulation or calculation
+
+    // Optimize Event handling
+    // Be mindful of event subscriptions and unregister them when no longer needed
+    // Excessive event handling can impact performance, so ensure you only subscribe to events that are necessary
+
+    // Optimize style sheets
+    // Large style sheets with numerous rules can impact performance
+
+    // Profile and optimize
+    // Unitys profiling tools to identify bottlenecks in your UI and spot areas that can be optimized
+
+    // Test in all target plataforms
+   
 }
