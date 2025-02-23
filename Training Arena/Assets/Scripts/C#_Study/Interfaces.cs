e4072699c5cf2c58af8eb55a075247f397db3953
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-23 18:54:17 -0300

    v184
    More about interfaces

--- Training Arena/Assets/Scripts/C#_Study/Interfaces.cs
+++ Training Arena/Assets/Scripts/C#_Study/Interfaces.cs
@@ -5,6 +5,13 @@ public class Interfaces : MonoBehaviour
     // Extremely powerful freature to make your code more modular
 
     // Interfaces are contratcs that a class can implement
+    // Allow us to define functions and properties, but not the implementation
+    // When a Class implements an interface it must implement those functions
+    // We always put I before the name of the interface
+    // We can define a property like int myInt { get; set; }
+    // For use interfaces, we put the interface on different objects, and where you have an action...
+    // create a function for get the Interface component and call the function on interface
+     
 
     private void Start()
     {
