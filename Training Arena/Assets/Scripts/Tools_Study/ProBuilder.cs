65bc392e0f71f4ff196795f095d0ae41d71090a4
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-24 07:28:57 -0300

    v187
    Probuilder cut mesh

--- Training Arena/Assets/Scripts/Tools_Study/ProBuilder.cs
+++ Training Arena/Assets/Scripts/Tools_Study/ProBuilder.cs
@@ -8,5 +8,14 @@ public class ProBuilder : MonoBehaviour
     // the most crazy about this powerfull tool is that we can modify 3d models that already have created on other software, just ProBuilderize it  
     // The uv editor tool is also very useful to modify the UVs of the models
 
+    // Destroy and slice objects inside Unity
+    // On preferences, we have the probuilder section, toggle the option to enable experimental features
 
+    // We have 3 modes: Intersection, union and subtraction
+
+    // Intersection: we can select 2 objects and make a new object with the intersection of the 2 objects
+    // Union: we make a mix of the 2 objects
+    // Subtraction: we remove the object 2 from object 1
+
+    // when we make this, we need to set the pivot of the object, because we have some bugs
 }
