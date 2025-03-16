2397ccecceffee6694048f910649e9335a7ab8d0
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-03-16 14:15:10 -0300

    Percentage
    v190

---
+++ Training Arena/Assets/Scripts/C#_Study/Percentage.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Percentage : MonoBehaviour
+{
+    // percentage = (part / total) * 100
+
+    // For example, 20 students in a classroom with 50 students, pass on the exam
+    // ( part (20) / total (50) ) * 100 = 40% - 40% of the students passed the exam
+
+
+    // We can calculate the part based on the percentage and the total
+
+    // for example, if one item represents 30% of the total, and the total is 100
+    // part = (30 / 100) * 100 = 30, so 30% of 100 is 30
+
+
+    // We can calculate the percentual increase or decrease between two values
+    // new value = old value * (1 + increase / 100) or (1 - decrease/100);
+
+    // for example, if the old value is 100 and the increase is 10%
+    // new value = 100 * (1 + 10 / 100) = 110
+
+    // We can calculate the percentual variation between two values
+    // variation = ((final value - inicial value) / inicial value) * 100
+
+
+}
